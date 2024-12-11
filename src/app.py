64338de1b2174719acfe9652fd525d3d7b5eb261
Bimal Kumar Shrestha <bimal.stha89@gmail.com>
2024-12-11 14:23:48 -0500

    Streamlit added

---
+++ src/app.py
@@ -0,0 +1,41 @@
+import streamlit as st
+import webbrowser
+import helper
+
+st.title('Food Classifier :hamburger::pizza::spaghetti:  ')
+
+st.write('Take a picture of the food and let us handle form there.')
+
+# Image input
+img_file_buffer = st.camera_input("Take a picture of the food.")
+image = None
+if img_file_buffer is not None:
+    image = img_file_buffer
+if not image:
+    uploaded_file = st.file_uploader("Upload an image", type=['png', 'jpg', 'jpeg', 'webp'])
+    if uploaded_file:
+        image = uploaded_file
+        st.image(uploaded_file)
+
+if image:
+    food,prob = helper.predict_image(image)
+    if prob<0.3:
+        st.write(f'Food: {food} \n Probability: {prob:.2f}')
+        st.warning('Image might not be a food or model might not know the food.', icon="⚠️")
+    else:
+        st.write(f'Food in the image: {food}\n Probability: {prob:.2f}')
+        st.write(f'Hey there! Do you want to make your own {food}?')
+
+def search_recipe(food):
+    food= food.replace('_','+')
+    webbrowser.open(f'https://www.google.com/search?q={food}+recipe&ie=UTF-8&oe=UTF-8')  # Go to example.com
+
+
+if st.button(' Get Recipe '):
+    search_recipe(food)
+
+
+footer_html = """<div style='margin-top:40px'>
+  <p>Developed with ❤️ ❤️ by Team NoName.</p>
+</div>"""
+st.markdown(footer_html, unsafe_allow_html=True)
---
+++ src/helper.py
@@ -0,0 +1,67 @@
+import tensorflow as tf
+
+import numpy as np
+import tensorflow as tf
+
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.image import img_to_array, load_img, array_to_img
+
+model_mobile_net = load_model("models/best_mobile_net_v2.keras")
+labels = []
+with open("data/meta/labels.txt", "r") as grilled_cheese:
+    lines = grilled_cheese.readlines()
+    for l in lines:
+        labels.append(l)
+
+
+def preprocess_new_image(image_path, input_shape):
+    """
+    Preprocesses a new image the same way as the training data.
+
+    Args:
+    - image_path (str): Path to the image file to preprocess.
+    - input_shape (tuple): Target size (height, width) for resizing the image.
+
+    Returns:
+    - np.array: Preprocessed image ready for prediction.
+    """
+    # Load the image
+    img = load_img(image_path, target_size=input_shape)
+
+    # Convert the image to a numpy array
+    img_array = img_to_array(img)
+
+    # Expand dimensions to match the batch shape (batch_size, height, width, channels)
+    img_array = np.expand_dims(img_array, axis=0)
+
+    # Preprocess the image using MobileNetV2 preprocessing function
+    img_array = tf.keras.applications.mobilenet_v2.preprocess_input(img_array)
+
+    return img_array
+
+
+def predict_image(image, input_shape=(224, 224)):
+    """
+    Predicts the class of a new image using the trained model.
+
+    Args:
+    - model (tf.keras.Model): The trained model for prediction.
+    - image_path (str): Path to the image to predict.
+    - input_shape (tuple): Target size (height, width) for resizing the image.
+
+    Returns:
+    - str: Predicted class label.
+    """
+    # Preprocess the image
+    preprocessed_image = preprocess_new_image(image, input_shape)
+
+    predictions = model_mobile_net.predict(preprocessed_image)
+
+    # Get the index of the class with the highest probability
+    predicted_class_index = np.argmax(predictions, axis=1)[0]
+
+    # Get the predicted class label and its probability
+    predicted_class = labels[predicted_class_index]
+    predicted_probability = predictions[0][predicted_class_index]
+    return predicted_class,predicted_probability
+
